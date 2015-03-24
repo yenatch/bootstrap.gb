df45f3739a5557d60b8f0e5fb5286be4b3e503fe
yenatch <yenatch@gmail.com>
2015-03-24 15:45:22 -0700

    joypad

---
+++ src/code/joypad.asm
@@ -0,0 +1,58 @@
+D_PAD   = $10
+BUTTONS = $20
+DONE    = $30
+
+
+section "joypad", rom0
+
+Joypad::
+	put [rJOYP], D_PAD
+	rept 2
+	ld a, [rJOYP]
+	endr
+
+	cpl
+	and %1111
+	swap a
+
+	ld b, a
+
+	put [rJOYP], BUTTONS
+	rept 6
+	ld a, [rJOYP]
+	endr
+
+	cpl
+	and %1111
+	or b
+
+	ld b, a
+
+	put [rJOYP], DONE
+
+	ld a, [wJoy]
+	ld [wJoyLast], a
+	ld e, a
+	xor b
+	ld d, a
+
+;	ld a, d
+	and e
+	ld [wJoyReleased], a
+
+	ld a, d
+	and b
+	ld [wJoyPressed], a
+
+	ld a, b
+	ld [wJoy], a
+
+	ret
+
+
+section "joypad wram", wram0
+
+wJoy::         db
+wJoyLast::     db
+wJoyPressed::  db
+wJoyReleased:: db
