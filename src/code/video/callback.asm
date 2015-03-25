ba97402f879e733b58cdbad99d33cb72b4613649
yenatch <yenatch@gmail.com>
2015-03-24 20:15:24 -0700

    Pass bc and de into callbacks.

--- src/code/video/callback.asm
+++ src/code/video/callback.asm
@@ -1,5 +1,5 @@
 NUM_CALLBACKS EQU 5
-CALLBACK_LENGTH EQU 3
+CALLBACK_LENGTH EQU 7
 
 
 section "callback wram", wram0
@@ -12,7 +12,7 @@ wCallbacks:
 section "callback", rom0
 
 Callback::
-; Call a:hl during the next vblank.
+; Call a:hl during the next vblank. Pass in bc and de.
 
 	push bc
 	push de
@@ -26,7 +26,7 @@ Callback::
 	and a
 	jr z, .ok
 	cp NUM_CALLBACKS
-	jr nc, .done
+	jr nc, .exit
 
 	push bc
 	ld bc, CALLBACK_LENGTH
@@ -44,30 +44,82 @@ Callback::
 	ld [hl], d
 	inc hl
 
+	pop de
+	pop bc
+
+	ld [hl], b
+	inc hl
+	ld [hl], c
+	inc hl
+	ld [hl], d
+	inc hl
+	ld [hl], e
+	inc hl
+
 	ld a, [wCallbacks]
 	inc a
 	ld [wCallbacks], a
-	jr .done
+	ret
 
-.done
+.exit
 	pop de
 	pop bc
 	ret
+
+
+;RunCallback:
+;	ld hl, wCallbacks
+;	ld a, [hli]
+;	and a
+;	ret z
+;
+;	put [wFarCallBank], [hli]
+;	put [wFarCallAddress + 0], [hli]
+;	put [wFarCallAddress + 1], [hli]
+;	put b, [hli]
+;	put c, [hli]
+;	put d, [hli]
+;	put e, [hli]
+;
+;	ld hl, wFarCallAddress
+;	ld a, [hli]
+;	ld h, [hl]
+;	ld l, a
+;
+;	ld a, [hROMBank]
+;	push af
+;	ld a, [wFarCallBank]
+;	rst Bankswitch
+;
+;	call __hl__
+;
+;	pop af
+;	rst Bankswitch
+;
+;	ret
 
 
 RunCallbacks:
 	ld hl, wCallbacks
 	ld a, [hli]
 	and a
-	jr z, .done
+	ret z
+
 .loop
 	push af
 
-	ld a, [hli]
-	ld [wFarCallBank], a
-	ld a, [hli]
+	put [wFarCallBank], [hli]
+	put [wFarCallAddress + 0], [hli]
+	put [wFarCallAddress + 1], [hli]
+	put b, [hli]
+	put c, [hli]
+	put d, [hli]
+	put e, [hli]
 
 	push hl
+
+	ld hl, wFarCallAddress
+	ld a, [hli]
 	ld h, [hl]
 	ld l, a
 
@@ -82,11 +134,9 @@ RunCallbacks:
 	rst Bankswitch
 
 	pop hl
-	inc hl
-
 	pop af
 	dec a
 	jr nz, .loop
+
 	ld [wCallbacks], a
-.done
 	ret
