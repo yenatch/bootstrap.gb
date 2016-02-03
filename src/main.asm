af19a42b3b9dbfe33273ca2e30d13a71b389a69c
yenatch <yenatch@gmail.com>
2016-02-02 17:01:41 -0800

    Trim down main.asm.

--- src/main.asm
+++ src/main.asm
@@ -1,30 +1,39 @@
 include "constants.asm"
 
 
-section "Hello World", rom0
+START_TILE = $80 ; This maps to vChars1.
+
+; All values are in tiles.
+IMAGE_WIDTH = 10
+IMAGE_HEIGHT = 4
+IMAGE_X = 5
+IMAGE_Y = 7
 
 
-HELLO_WORLD_START_TILE = $80 ; VChars1
+section "Main", rom0
 
+Main::
+	call .Setup
+.loop
+	call WaitVBlank
+	jr .loop
 
-HelloWorld:
-	ld bc, HelloWorldGfx
+.Setup:
+	ld bc, .Graphics
 	ld de, vChars1
-	ld a, 10 * 4
+	ld a, IMAGE_WIDTH * IMAGE_HEIGHT
 	call QueueGfx
 
-	ld de, vBGMap0 + BG_WIDTH * 7 + 5
-	ld b, 4
-	ld c, 10
-	callback DrawHelloTilemap
+	ld de, vBGMap0 + BG_WIDTH * IMAGE_Y + IMAGE_X
+	ld b, IMAGE_HEIGHT
+	ld c, IMAGE_WIDTH
+	callback .DrawTilemap
 
-	call NormalPals
+	call .SetPalette
 
 	ret
 
-
-DrawHelloTilemap:
-
+.DrawTilemap:
 	ld h, d
 	ld l, e
 
@@ -33,7 +42,7 @@ DrawHelloTilemap:
 	sub c
 	ld e, a
 
-	ld a, HELLO_WORLD_START_TILE
+	ld a, START_TILE
 .y
 	push bc
 .x
@@ -48,97 +57,13 @@ DrawHelloTilemap:
 	jr nz, .y
 	ret
 
-
-HelloWorldGfx:
+.Graphics:
 	INCBIN "gfx/hello_world.2bpp"
 
-
-NormalPals:
+.SetPalette:
 	ld a, %11100100 ; quaternary: 3210
 	ld [rOBP0], a
 	ld [rOBP1], a
 	ld [rBGP], a
 	ret
 
-
-
-section "Main", rom0
-
-
-Main::
-	call HelloWorld
-.loop
-	call Joypad
-	call Scroll
-	call RequestFrame
-	jr .loop
-
-
-RequestFrame:
-	callback UpdateScroll
-	call WaitVBlank
-	ret
-
-
-UpdateScroll:
-	put [rSCY], [wSCY]
-	put [rSCX], [wSCX]
-	ret
-
-
-Scroll:
-
-if_button: macro
-	ld a, [wJoy]
-	and \1
-	jr z, .ok\@
-	\2
-.ok\@
-endm
-
-	put b, [wSCY]
-	put c, [wSCX]
-
-	ld a, [wScrollReset]
-	and a
-	jr nz, .reset
-
-	if_button START, jr .start
-
-	if_button D_UP,    inc b
-	if_button D_DOWN,  dec b
-	if_button D_LEFT,  inc c
-	if_button D_RIGHT, dec c
-	jr .done
-
-.start
-	put [wScrollReset], 10
-	jr .done
-
-.reset
-	dec a
-	ld [wScrollReset], a
-
-	ld a, b
-	and %10000000 ; sign
-	srl b
-	or b
-	ld b, a
-
-	ld a, c
-	and %10000000 ; sign
-	srl c
-	or c
-	ld c, a
-
-.done
-	put [wSCY], b
-	put [wSCX], c
-	ret
-
-
-section "main wram", wram0
-
-wSCY: ds 1
-wSCX: ds 1
-wScrollReset: ds 1
